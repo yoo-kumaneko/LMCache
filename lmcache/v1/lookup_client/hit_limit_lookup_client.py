2a3673e246fe01abda6a54111e7e5a71686c9e64
idellzheng <idellzheng@tencent.com>
2025-12-30 13:17:58 +0800

    Revert "Optimize lookups in sync mode by skipping cached chunks. (#2111)"
    This reverts commit 2febf17972955fc3832d78cb587db1be93ba5b70.

--- lmcache/v1/lookup_client/hit_limit_lookup_client.py
+++ lmcache/v1/lookup_client/hit_limit_lookup_client.py
@@ -41,15 +41,9 @@ class HitLimitLookupClient(LookupClientInterface):
         token_ids: Union[torch.Tensor, list[int]],
         lookup_id: str,
         request_configs: Optional[dict] = None,
-        num_computed_tokens: int = 0,
     ) -> Optional[int]:
         # get real hit tokens
-        result = self.actual_lookup_client.lookup(
-            token_ids,
-            lookup_id,
-            request_configs,
-            num_computed_tokens,
-        )
+        result = self.actual_lookup_client.lookup(token_ids, lookup_id, request_configs)
         if result is not None:
             total_tokens_length = len(token_ids)
             assert result <= total_tokens_length
